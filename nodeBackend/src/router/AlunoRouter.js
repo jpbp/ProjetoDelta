6601300d7e91e496ecc88b382e0391276d116e91
jpbp <jpbp@estudante.ufla.br>
2020-02-12 15:00:16 -0300

    configurando rota da lista de alunos

--- nodeBackend/src/router/AlunoRouter.js
+++ nodeBackend/src/router/AlunoRouter.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const AlunosController=require('../controllers/AlunoController')
-router.get('/datatest',AlunosController.datatest)
-router.get('/test',AlunosController.test)
-router.get('/save',(req,res)=>res.json({status:'Aluno salvo'}))
+
+router.get('/list',AlunosController.list)
+
+//teste do programa
+// router.get('/datatest',AlunosController.datatest)
+// router.get('/test',AlunosController.test)
+// router.get('/save',(req,res)=>res.json({status:'Aluno salvo'}))
 
 module.exports=router
