4458b51369a14520afec9cb0c17e88a015ac37e7
jpbp <jpbp@estudante.ufla.br>
2020-02-12 15:08:38 -0300

    configurando rota da lista de alunos para exibição dos dados

--- nodeBackend/src/controllers/AlunoController.js
+++ nodeBackend/src/controllers/AlunoController.js
@@ -8,36 +8,49 @@ const sequelize = require('../model/database')
 const Aluno = require('../model/Aluno')
 
 
-controllers.datatest= async(req,res)=>{
-    const response = await sequelize.sync().then(function(){
-        fs.openSync('C:\desafioDelta.jpeg')
-        const img = fs.readFileSync('C:\desafioDelta.jpeg')
-        console.log('foi')
-        Aluno.create({
-            id:9,
-            nome:'João Paulo',
-            endereco:'Rua joaquim manoel',
-            imagem: img
-        }).catch(error=>{
-            return error
-        })
-        
+sequelize.sync()
 
-        const data= Aluno.findAll()
+controllers.list=async(req,res)=>{
+    const data = await Aluno.findAll().then(function(data){
         return data
     }).catch(error=>{
-        console.log(error)
         return error
-    })
-    res.json({sucess:true, data:response})
-}
-controllers.test = (req,res)=>{
-    const data = {
-        name:'João Paulo',
-        age:22,
-        email:"jpbp@gmail.com"
     }
-    console.log('Executando do controle')
-    res.json(data)
+    )
+    res.json({sucess:true,data:data})
 }
+
+//teste do prograa
+// controllers.datatest= async(req,res)=>{
+//     const response = await sequelize.sync().then(function(){
+//         fs.openSync('C:\desafioDelta.jpeg')
+//         const img = fs.readFileSync('C:\desafioDelta.jpeg')
+//         console.log('foi')
+//         Aluno.create({
+//             id:9,
+//             nome:'João Paulo',
+//             endereco:'Rua joaquim manoel',
+//             imagem: img
+//         }).catch(error=>{
+//             return error
+//         })
+        
+
+//         const data= Aluno.findAll()
+//         return data
+//     }).catch(error=>{
+//         console.log(error)
+//         return error
+//     })
+//     res.json({sucess:true, data:response})
+// }
+// controllers.test = (req,res)=>{
+//     const data = {
+//         name:'João Paulo',
+//         age:22,
+//         email:"jpbp@gmail.com"
+//     }
+//     console.log('Executando do controle')
+//     res.json(data)
+// }
 module.exports=controllers
