42e347167075cf581c39cc043e9d6b9a5510303a
jpbp <jpbp@estudante.ufla.br>
2020-02-12 00:29:03 -0300

    configurando servidor

---
+++ nodeBackend/src/App.js
@@ -0,0 +1,15 @@
+// importando express 
+const express = require ('express')
+const app = express()
+
+//configurando as rotas 
+
+app.set('port',process.env.POST||3000)
+
+// Middlewares 
+
+app.use(express.json())
+
+app.use('/',(req,res)=>res.send('Hello World'))
+
+app.listen(app.get('port'),()=>console.log('Iniciado Servidor'))
