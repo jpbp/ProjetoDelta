f372c8e665b42724a24e019e2f6e6efb4ef022ad
jpbp <jpbp@estudante.ufla.br>
2020-02-12 00:50:10 -0300

    configurando rota do controller

--- nodeBackend/src/router/AlunoRouter.js
+++ nodeBackend/src/router/AlunoRouter.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const AlunosController=require('../controllers/AlunoController')
+router.get('/test',AlunosController.test)
 router.get('/save',(req,res)=>res.json({status:'Aluno salvo'}))
 
 module.exports=router
