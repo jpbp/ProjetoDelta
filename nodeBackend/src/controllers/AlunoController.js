9d0ddbc1e0c8398fcdbd710a9f3a9df064d4b883
jpbp <jpbp@estudante.ufla.br>
2020-02-12 01:22:30 -0300

    configurando a rota do aluno para sincronização do banco de dados

--- nodeBackend/src/controllers/AlunoController.js
+++ nodeBackend/src/controllers/AlunoController.js
@@ -1,5 +1,18 @@
 const controllers = {}
 
+// importando modelo e sequelize
+
+const sequelize = require('../model/database')
+const Aluno = require('../model/Aluno')
+
+controllers.teste=(req,res)=>{
+    const response = sequelize.sync().then(()=>{
+        const data = Aluno.findAll()
+        return data
+    }).catch(error=>error)
+    res.json(response) 
+}
+
 controllers.test = (req,res)=>{
     const data = {
         name:'João Paulo',
