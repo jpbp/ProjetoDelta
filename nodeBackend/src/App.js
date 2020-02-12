021cfb8e2cf9ac2334f05b99bb78ff481407ae29
jpbp <jpbp@estudante.ufla.br>
2020-02-12 15:24:48 -0300

    Configuração do cabeçalho e do cors

--- nodeBackend/src/App.js
+++ nodeBackend/src/App.js
@@ -11,6 +11,17 @@ app.set('port',process.env.POST||3000)
 app.use(express.json())
 
 
+// Configurar cabeçalhos e cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
+
 //importando rotas
 
 const alunoRouters = require('./router/AlunoRouter')
